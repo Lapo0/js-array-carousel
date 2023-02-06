714d66c5eb3084402848d8adf331cdb9f4b32e0d
Lapo0 <lapo.gini.01@gmail.com>
2023-02-06 16:14:44 +0100

    FEAT post-hotwell

--- js/app.js
+++ js/app.js
@@ -3,15 +3,26 @@
 const slideElements = document.getElementsByClassName('slide')
 console.log(slideElements)
 
-let leftArrow = document.getElementsByClassName('left-arrow')
-let rightArrow = document.getElementsByClassName('right-arrow')
+let slideAttiva = 0
+
+let leftArrow = document.querySelector('.left-arrow')
+let rightArrow = document.querySelector('.right-arrow')
 
 console.log(leftArrow, rightArrow)
 
 rightArrow = addEventListener('click', function () {
-    console.log('funziona il destro')
+
+    // Rimuovere classe active
+    let slideCorrente = slideElements[slideAttiva]
+    slideCorrente.classList.remove('active')
+
+    slideAttiva += 1
+
+    // Aggiungere classe active
+    let slideSuccessiva = slideElements[slideAttiva]
+    slideSuccessiva.classList.add('active')
 })
 
 leftArrow = addEventListener('click', function () {
-    console.log('funziona il sinistro')
+
 })
