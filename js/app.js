8ddf614159ca6407074a0e5696966de194e46f56
Lapo0 <lapo.gini.01@gmail.com>
2023-02-06 16:38:19 +0100

    FEAT left & right carusel

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ let rightArrow = document.querySelector('.right-arrow')
 
 console.log(leftArrow, rightArrow)
 
-rightArrow = addEventListener('click', function () {
+rightArrow.addEventListener('click', function () {
 
     // Rimuovere classe active
     let slideCorrente = slideElements[slideAttiva]
@@ -23,6 +23,14 @@ rightArrow = addEventListener('click', function () {
     slideSuccessiva.classList.add('active')
 })
 
-leftArrow = addEventListener('click', function () {
+leftArrow.addEventListener('click', function () {
+    // Rimuovere classe active
+    let slideCorrente = slideElements[slideAttiva]
+    slideCorrente.classList.remove('active')
 
+    slideAttiva -= 1
+
+    // Aggiungere classe active
+    let slideSuccessiva = slideElements[slideAttiva]
+    slideSuccessiva.classList.add('active')
 })
