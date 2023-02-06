a23a205aeafb9b53f59666113ffb77d6a7b49f2d
Lapo0 <lapo.gini.01@gmail.com>
2023-02-06 17:40:16 +0100

    FEAT js DOM & comment HTML

--- js/app.js
+++ js/app.js
@@ -1,3 +1,65 @@
+// HTML RIPRODOTTO IN JS
+
+// ADD SECTION
+const carosello = document.createElement('section');
+
+// ADD CONTAINER
+const container = document.createElement('div');
+container.classList.add('container-sm', 'py-5');
+// feat container in section
+carosello.appendChild(container);
+
+// ARRAY IMMAGINI
+const images = [
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgfh73DIdzzaYX8LxezWX-G4G2uFA4-78bAA&usqp=CAU',
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8CSuKAbbfXdwNJqZlrFJ6t5opoOPNYEiZ_g&usqp=CAU',
+  'https://images.unsplash.com/photo-1580757468214-c73f7062a5cb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8MTYlM0E5fGVufDB8fDB8fA%3D%3D&w=1000&q=80',
+  'https://picsum.photos/536/354'
+];
+
+// CICLO CARUSEL
+for (let i = 0; i < images.length; i++) {
+
+    // Div Slide
+  const slide = document.createElement('div');
+  slide.classList.add('slide', 'none');
+  if (i === 0) {
+    slide.classList.add('active');
+  }
+  // feat slide in container
+  container.appendChild(slide);
+
+  // Img Slide
+  const img = document.createElement('img');
+  img.src = images[i];
+  img.alt = '';
+  // feat img in slide
+  slide.appendChild(img);
+}
+
+// ARROW
+// LEFT ARROW
+const leftArrow = document.createElement('div');
+leftArrow.classList.add('arrow', 'left-arrow');
+const leftArrowIcon = document.createElement('i');
+leftArrowIcon.classList.add('fa-solid', 'fa-chevron-left');
+// feat 'leftArrow' div in container & feat icon in 'leftArrow' div
+leftArrow.appendChild(leftArrowIcon);
+container.appendChild(leftArrow);
+
+// RIGHT ARROW
+const rightArrow = document.createElement('div');
+rightArrow.classList.add('arrow', 'right-arrow');
+const rightArrowIcon = document.createElement('i');
+rightArrowIcon.classList.add('fa-solid', 'fa-chevron-right');
+// feat 'rightArrow' div in container & feat icon in 'rightArrow' div
+rightArrow.appendChild(rightArrowIcon);
+container.appendChild(rightArrow);
+
+// feat carusel in body html
+document.body.appendChild(carosello);
+
+
 // CLICK SULL'ICONA
 
 const slideElements = document.getElementsByClassName('slide')
@@ -5,32 +67,38 @@ console.log(slideElements)
 
 let slideAttiva = 0
 
-let leftArrow = document.querySelector('.left-arrow')
-let rightArrow = document.querySelector('.right-arrow')
+// let leftArrow = document.querySelector('.left-arrow')
+// let rightArrow = document.querySelector('.right-arrow')
 
 console.log(leftArrow, rightArrow)
 
 rightArrow.addEventListener('click', function () {
 
-    // Rimuovere classe active
-    let slideCorrente = slideElements[slideAttiva]
-    slideCorrente.classList.remove('active')
+    if (slideAttiva < slideElements.length - 1) {
+        // Rimuovere classe active
+        let slideCorrente = slideElements[slideAttiva]
+        slideCorrente.classList.remove('active')
 
-    slideAttiva += 1
+        slideAttiva += 1
 
-    // Aggiungere classe active
-    let slideSuccessiva = slideElements[slideAttiva]
-    slideSuccessiva.classList.add('active')
+        // Aggiungere classe active
+        let slideSuccessiva = slideElements[slideAttiva]
+        slideSuccessiva.classList.add('active')
+    }
 })
 
 leftArrow.addEventListener('click', function () {
+
+    if (slideAttiva > 0) {
     // Rimuovere classe active
-    let slideCorrente = slideElements[slideAttiva]
-    slideCorrente.classList.remove('active')
+        let slideCorrente = slideElements[slideAttiva]
+        slideCorrente.classList.remove('active')
 
-    slideAttiva -= 1
+        slideAttiva -= 1
 
-    // Aggiungere classe active
-    let slideSuccessiva = slideElements[slideAttiva]
-    slideSuccessiva.classList.add('active')
+        // Aggiungere classe active
+        let slideSuccessiva = slideElements[slideAttiva]
+        slideSuccessiva.classList.add('active')
+    }
 })
+
